def93b3f910f5865d6e76e39b9758062d9f9f42e
Gangwei XU <gwxu@hust.edu.cn>
2024-12-29 22:52:24 +0800

    Add files via upload

---
+++ IGEV-Stereo/env_bfloat16.sh
@@ -0,0 +1,7 @@
+conda install pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.7 -c pytorch -c nvidia
+pip install opencv-python==4.9.0.80
+pip install tensorboard==2.14.0
+pip install scikit-image==0.21.0
+pip install timm==0.5.4
+pip install matplotlib==3.7.4
+pip install numpy==1.24.3
