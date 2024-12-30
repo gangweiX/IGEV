43193a4237727c7f48f61ce0a5b642eb016dc3cf
Gangwei XU <gwxu@hust.edu.cn>
2024-12-30 14:00:32 +0800

    Add files via upload

---
+++ IGEV-Stereo/env.sh
@@ -0,0 +1,8 @@
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install tqdm
+pip install scipy
+pip install opencv-python
+pip install scikit-image
+pip install tensorboard
+pip install matplotlib 
+pip install timm==0.5.4
