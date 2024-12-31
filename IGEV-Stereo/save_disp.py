dff4af81ed855c73c32aff544d9b3bb24f330f61
Gangwei XU <gwxu@hust.edu.cn>
2024-12-31 11:00:52 +0800

    Update save_disp.py

--- IGEV-Stereo/save_disp.py
+++ IGEV-Stereo/save_disp.py
@@ -70,6 +70,7 @@ if __name__ == '__main__':
     # parser.add_argument('-r', '--right_imgs', help="path to all second (right) frames", default="/data/KITTI/KITTI_2012/testing/colored_1/*_10.png")
     parser.add_argument('--output_directory', help="directory to save output", default="output/kitti2015/disp_0")
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
+    parser.add_argument('--precision_dtype', default='float32', choices=['float16', 'bfloat16', 'float32'], help='Choose precision type: float16 or bfloat16 or float32')
     parser.add_argument('--valid_iters', type=int, default=16, help='number of flow-field updates during forward pass')
 
     # Architecture choices
